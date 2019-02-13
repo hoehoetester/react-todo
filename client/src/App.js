18e15801dcf08db95d9221a4f1ec1d0617ddcf16
Tommy Tsuda <tommy.tsuda@trademe.co.nz>
2019-02-13 22:04:23 +1300

    add: added sass settings(node-sass)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import axios from 'axios';
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   componentDidMount() {
